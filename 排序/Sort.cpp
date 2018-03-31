e221e5466d37e08918ad921c387996fa21352798
Dante <337544818@qq.com>
2018-03-31 20:18:27 +0800

    The 6 days

--- 排序/Sort.cpp
+++ 排序/Sort.cpp
@@ -207,7 +207,7 @@ void Merge(int r[], int r1[], int s, int m, int t)
       else  
 		  while (j<=t)                  //若第二个子序列没处理完，则进行收尾处理        
 			r1[k++]=r[j++]; 
-}
+} 
 
 
 //一趟归并
@@ -263,3 +263,28 @@ void MergeSort2(int r[], int r1[], int r2[],int s, int t)
             Merge(r2, r1, s, m, t);             //将两个已排序的子序列归并 		
 	}	 
 }
+
+
+void quicksort(int arr[], int l, int r)
+{
+	if (l<r)
+	{
+		int pivot = partition(arr, l, r);
+		quicksort(arr, l, pivot-1);
+		quicksort(arr, pivot + 1, r);
+	}
+}
+int partition(int arr[], int l, int r)
+{
+	int k = l, pivot = arr[r];
+	for (int i=l;i<r;++i)
+	{
+		if (arr[i]<pivot)
+		{
+			swap(arr[i], arr[k++]);
+		}
+	}
+	swap(arr[k], arr[r]);
+	return k;
+}
+//从头到尾迭代，和支点比较，大的不管，小的换。换了才往后看。最后支点戳中间，算是分界线。
